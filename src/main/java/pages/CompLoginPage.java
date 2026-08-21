267187649f2853e61c3a120d639e29f229ccb114
Murali0940 <Murali0940@gmail.com>
2026-08-21 13:12:18 +0530

    test

--- src/main/java/pages/CompLoginPage.java
+++ src/main/java/pages/CompLoginPage.java
@@ -2,9 +2,14 @@ package pages;
 
 import com.microsoft.playwright.*;
 
+import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CompLoginPage {
 
     private Page page;
+    private static final Logger logger = LogManager.getLogger(CompLoginPage.class);
 
     private Locator txtUsername;
     private Locator txtPassword;
@@ -26,12 +31,18 @@ public class CompLoginPage {
 
         txtUsername.fill(user);
         System.out.println("Company username is: " + user);
+        Allure.step("Company username is: " + user);
+        logger.info("Company username is: " + user);
 
         txtPassword.fill(pass);
         System.out.println("Company password is: " + pass);
+        Allure.step("Company password is: " + pass);
+        logger.info("Company password is: " + pass);
 
         btnLogin.click();
         System.out.println("Company login button clicked.");
+        Allure.step("Company login button clicked.");
+        logger.info("Company login button clicked.");
 
         return new UserLoginPage(page);
 
--- src/test/java/tests/Rough.java
+++ src/test/java/tests/Rough.java
@@ -34,4 +34,10 @@ public class Rough extends BaseDriver {
 		new Homepage(page).search_File_Root_Location(".pdf", "File");
 	}
 
+	@Test(priority = 3, description = "search_Folder_Root_Location", enabled = true)
+	public void search_Root_Location_Folder() {
+
+		new Homepage(page).search_Folder_Root_Location("test", "Folder");
+	}
+
 }
